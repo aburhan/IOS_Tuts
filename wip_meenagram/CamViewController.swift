6cc19859d89c4f40149fd739fcda87a5cef28fd0
aburhan <ameenah.burhan@gmail.com>
2017-02-23 19:40:15 -0500

    add camera to app to take pictures

--- wip_meenagram/CamViewController.swift
+++ wip_meenagram/CamViewController.swift
@@ -5,6 +5,7 @@
 //
 
 import UIKit
+import Firebase
 
 class CamViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -17,7 +18,7 @@ class CamViewController: UIViewController, UIImagePickerControllerDelegate, UINa
 
         // Do any additional setup after loading the view.
     }
-
+//UI Button Actions
     @IBAction func sharePhoto(_ sender: Any) {
     }
     @IBAction func cancelUpload(_ sender: Any) {
@@ -29,6 +30,8 @@ class CamViewController: UIViewController, UIImagePickerControllerDelegate, UINa
     @IBAction func takePhotoAction(_ sender: Any) {
         takeNewPhoto()
     }
+    //end of UI Button Actions
+    //get a photo from our devices library
     func getPhotoFromLibrary(){
         picker.delegate = self
         picker.allowsEditing = false
@@ -36,14 +39,33 @@ class CamViewController: UIViewController, UIImagePickerControllerDelegate, UINa
         
         present(picker, animated: true, completion: nil)
     }
+    //camera for new photo
     func takeNewPhoto(){
-        
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = UIImagePickerControllerCameraCaptureMode.photo
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+        }else{
+            noCameraAvailable()
+        }
     }
+    func noCameraAvailable(){
+        let alertVC = UIAlertController(title: "No Camera Available", message: "Can't find a camera on this device", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertVC.addAction(okAction)
+        present(alertVC, animated: true, completion: nil)
+    }
+    //function for image picker
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         postImageView.image = chosenImage
         dismiss(animated: true, completion: nil)
     }
+    //when user cancels the image picker
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         self.dismiss(animated: true, completion: nil)
     }
