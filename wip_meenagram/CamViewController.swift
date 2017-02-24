c1b645abf13e136b81879303f442fd9787193cbd
aburhan <ameenah.burhan@gmail.com>
2017-02-23 19:10:13 -0500

    adding ability to add photos to the storage

--- wip_meenagram/CamViewController.swift
+++ wip_meenagram/CamViewController.swift
@@ -6,11 +6,12 @@
 
 import UIKit
 
-class CamViewController: UIViewController {
+class CamViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var captionTextView: UITextView!
     
+    let picker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,9 +21,30 @@ class CamViewController: UIViewController {
     @IBAction func sharePhoto(_ sender: Any) {
     }
     @IBAction func cancelUpload(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     @IBAction func pickPhotoFromGallery(_ sender: Any) {
+        getPhotoFromLibrary()
     }
     @IBAction func takePhotoAction(_ sender: Any) {
+        takeNewPhoto()
+    }
+    func getPhotoFromLibrary(){
+        picker.delegate = self
+        picker.allowsEditing = false
+        picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+        
+        present(picker, animated: true, completion: nil)
+    }
+    func takeNewPhoto(){
+        
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        postImageView.image = chosenImage
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
     }
 }
